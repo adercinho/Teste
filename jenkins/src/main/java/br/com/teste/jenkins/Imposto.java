7bd02304530105993675258c0ae120cfb7a3de0c
Adercio Macedo <adercio.macedo@4linux.com.br>
2013-03-15 17:51:18 -0300

    Criado o TODO para inserção da logica!

--- jenkins/src/main/java/br/com/teste/jenkins/Imposto.java
+++ jenkins/src/main/java/br/com/teste/jenkins/Imposto.java
@@ -3,7 +3,7 @@ package br.com.teste.jenkins;
 public class Imposto {
 
 
-	public double calcula(Orcamento orcamento){
+	public static double calcula(Orcamento orcamento){
 		//TODO INSIRA A LOGICA AQUI
 		return 0.0;
 	}
