a5ee64d71ff9335bc6efd8f33ff8c2c3056b7f04
Adercio Macedo <adercio.macedo@4linux.com.br>
2013-03-18 09:50:09 -0300

    Criado o java doc para o metodo calcula do Imposto.

--- jenkins/src/main/java/br/com/teste/jenkins/Imposto.java
+++ jenkins/src/main/java/br/com/teste/jenkins/Imposto.java
@@ -3,6 +3,10 @@ package br.com.teste.jenkins;
 public class Imposto {
 
 
+	/**
+	 * @param orcamento
+	 * @return valor do imposto referente ao orçamento
+	 */
 	public static double calcula(Orcamento orcamento){
 		double resultado = orcamento.getValor() * 0.1;
 		return resultado;
